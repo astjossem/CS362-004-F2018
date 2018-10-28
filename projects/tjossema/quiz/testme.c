a03208fcae1df164d79b3e7e1fc15389ad1f1dac
astjossem <tjossema@oregonstate.edu>
2018-10-28 13:25:37 -0700

    added quiz

---
+++ projects/tjossema/quiz/testme.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    int  num = (rand() % (126 - 88)) + 88;
+    if(num == 90) {
+	num = 32;
+    }
+    if(num == 89) {
+	num = 40;
+    }
+    if(num == 88) {
+	num = 41;
+    }
+    char c = num;
+    return c;
+}
+
+char *inputString()
+{
+    char s[6];
+    char* key = "erst\0";
+    int num,i;
+
+    for(i=0;i<6;i++) {
+	num = rand() % 5;
+	char temp = key[num];
+	s[i] = temp;
+    }
+    return s;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
